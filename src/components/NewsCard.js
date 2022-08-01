54331bcaa30b7c18ef3a57c91cbea293f3c3e055
Prawoto Hadi <pawoto.hadi@gmail.com>
2022-08-01 19:25:11 +0700

    Bug Fixed

--- src/components/NewsCard.js
+++ src/components/NewsCard.js
@@ -4,7 +4,13 @@ import Link from '@mui/material/Link';
 import * as React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 
-const NewsCard = ({ news, index }) => {
+const NewsCard = ({ news, category, search, index }) => {
+  let queryParam=''
+  if (search) {
+    queryParam = `?q=${search}`
+  }
+  let date = news.publishedAt.split('T')
+  date = date[0].split('-')
   return (
     <Box sx={{ display: 'flex', width: 350, margin: 0, padding: '10px 15px', borderBottom: '1px solid #CCCCCC' }}>
       <CardMedia
@@ -16,10 +22,10 @@ const NewsCard = ({ news, index }) => {
       <Box sx={{ display: 'flex', flexDirection: 'column', ml: 2}}>
         <Box sx={{ flex: '1 0 auto', padding: 0 }}>
           <Typography component="div" variant="h6" sx={{lineHeight: 1, ms: 1}}>
-            <Link component={RouterLink} to={`/detail/popular/${index}`}>{news.title}</Link>
+            <Link component={RouterLink} to={`/detail/${category}/${index}${queryParam}`}>{news.title}</Link>
           </Typography>
           <Typography variant="subtitle1" color="text.secondary" component="div">
-            {new Date(news.publishedAt).getFullYear()}
+            {date[2] + '-' + date[1] + '-' + date[0]}
           </Typography>
         </Box>
       </Box>
